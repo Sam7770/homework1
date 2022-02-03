b448026f2d90ac68eb17d75cc224d58ab1f29e8d
Sam7770 <dimitri.puzikov@gmail.com>
2022-02-03 17:43:33 +0200

    homework. progress 5

--- JS projects/lesson1/classwork/main.js
+++ JS projects/lesson1/classwork/main.js
@@ -1,4 +1,68 @@
 // 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
 
-let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-let result
+let num10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+num10 =
+console.log(num10)
+
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+
+let book = {
+    name: 'Martian',
+    pages: 252,
+    genre: 'fiction',
+    authors: {
+        main:  'Joseph Winston',
+       second: 'Mark Rufallo',
+       third: 'Henry Stones'
+    }
+
+}
+console.log(book.authors.main)
+
+// Створити маcив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+
+let books = [ book1 = {
+    name: 'Flowers',
+    pages: 520,
+    genre: 'catastrophy',
+    authors: {
+        main:  'Joseph Rufallo',
+        second: 'Mark Winston',
+        third: 'Henry Winston'
+    }
+
+},
+    book2 = {
+    name: 'Water',
+    pages: 287,
+    genre: 'fiction',
+    authors: {
+        main:  'Henry Rufallo',
+        second: 'Mark Stones',
+        third: 'Joseph Stones'
+    }
+
+
+}
+]
+
+console.log(book1, book2)
+
+
+
+
+// Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+
+let height = 23;
+let width = 10
+
+console.log(height * width)
+
+let S = 230
+
+
+
+
